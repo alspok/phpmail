7809f5d04c3e2cbd5d7ad14e40899db0f003792a
alspok <alspok@gmail.com>
2018-11-15 20:34:46 +0200

    .

--- phpmail.php
+++ phpmail.php
@@ -10,8 +10,8 @@ require 'C:\wamp\www\alspok\vendor\autoload.php';
 $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
 try {
     //Server settings
-    $mail->SMTPDebug = 2;                                // Enable verbose debug output
-    $mail->Debugoutput = 'html';
+    // $mail->SMTPDebug = 2;                                // Enable verbose debug output
+    // $mail->Debugoutput = 'html';
     $mail->isSMTP();                                      // Set mailer to use SMTP
     // $mail->Host = 'smtp1.example.com;smtp2.example.com';  // Specify main and backup SMTP servers
     $mail->Host = 'smtp.mailtrap.io';
@@ -24,10 +24,11 @@ try {
     $mail->Port = 25;
 
     //Recipients
-    $mail->setFrom('a.spokauskas@outlook.com', 'Mailer');
-    // $mail->addAddress('alspok@gmail.com', 'ASpok');     // Add a recipient Name is optional
-    // $mail->addAddress('a.spokauskas@outlook.com', 'AS');
-    $mail->addAddress('test-sm35o@mail-tester.com');
+    $mail->setFrom('mailer@phpmailer.com', 'Mailer');
+    $mail->addAddress('alspok@gmail.com', 'ASpok');     // Add a recipient Name is optional
+    $mail->addAddress('a.spokauskas@outlook.com', 'AS');
+    $mail->addAddress('test-sm35o@mail-tester.com', 'Mailtrap');
+    // $mail->addAddress('a.spokauskas@outlook.com');
     // $mail->addAddress('ellen@example.com');
     // $mail->addReplyTo('info@example.com', 'Information');
     // $mail->addCC('cc@example.com');
@@ -39,8 +40,8 @@ try {
 
     //Content
     $mail->isHTML(true);                                  // Set email format to HTML
-    $mail->Subject = 'New subject';
-    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
+    $mail->Subject = 'New';
+    $mail->Body    = 'This is the HTML message body <i>in italic!</i>';
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
     $mail->send();
